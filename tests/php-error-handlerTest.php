1d77c63d55fc837215a47f3fa41a6c96188e66ad
unknown <Tyrael@.(none)>
2011-03-02 23:34:44 +0100

    fix phpunit config

---
+++ tests/php-error-handlerTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'src/php-error-handler.php';
+
+class phpErrorHandlerTest extends PHPUnit_Framework_TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
